5afb46935c30e436958dc8e0fccfa58a237ee64f
Mahmoud Salah <58883594+susano254@users.noreply.github.com>
2024-01-22 04:00:15 +0200

    first commit

--- pid_tuning/views.py
+++ pid_tuning/views.py
@@ -13,27 +13,27 @@ def home(request):
 
 def direct_param(request):
 	params[0] = float(request.GET.get('direct'))
-	# write_bytes(str(params).strip('[]'))
+	write_bytes(str(params).strip('[]'))
 	return HttpResponse("direct saved")
 
 def desired_param(request):
 	params[1] = float(request.GET.get('desired'))
-	# write_bytes(str(params).strip('[]'))
+	write_bytes(str(params).strip('[]'))
 	return HttpResponse("desired saved")
 
 def kp_param(request):
 	params[2] = float(request.GET.get('kp'))
-	# write_bytes(str(params).strip('[]'))
+	write_bytes(str(params).strip('[]'))
 	return HttpResponse("kp saved")
 
 def kd_param(request):
 	params[3] = float(request.GET.get('kd'))
-	# write_bytes(str(params).strip('[]'))
+	write_bytes(str(params).strip('[]'))
 	return HttpResponse("kd saved")
 
 def ki_param(request):
 	params[4] = float(request.GET.get('ki'))
-	# write_bytes(str(params).strip('[]'))
+	write_bytes(str(params).strip('[]'))
 	return HttpResponse("ki saved")
 
 # def serial_data(request):
